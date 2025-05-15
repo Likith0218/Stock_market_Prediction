38693c6c03c627ff2946a44efd2cb54f25747cbf
Mohan S Reddy <98205529+Mohan0230@users.noreply.github.com>
2025-05-15 13:38:32 +0530

    Update live_chart.py

--- live_chart.py
+++ live_chart.py
@@ -3,32 +3,35 @@
 import streamlit as st
 import streamlit.components.v1 as components
 
-def show_tradingview_chart():
+def show_tradingview_chart(ticker=None, timeframe='1d'):
     st.subheader("📈 TradingView Chart Integration")
 
     ticker = st.text_input("Enter Stock Ticker (e.g., NSE:TCS, NASDAQ:AAPL)", value="NSE:TCS")
 
-    chart_html = f"""
-    <div class="tradingview-widget-container">
-      <div id="tradingview_chart"></div>
-      <script type="text/javascript" src="https://s3.tradingview.com/tv.js"></script>
-      <script type="text/javascript">
-      new TradingView.widget({{
-        "width": "100%",
-        "height": 600,
-        "symbol": "{ticker}",
-        "interval": "1",
-        "timezone": "Etc/UTC",
-        "theme": "dark",
-        "style": "1",
-        "locale": "en",
-        "toolbar_bg": "#f1f3f6",
-        "enable_publishing": false,
-        "allow_symbol_change": true,
-        "container_id": "tradingview_chart"
-      }});
-      </script>
-    </div>
-    """
-
-    components.html(chart_html, height=620)
+    components.html(
+        f"""
+        <div class="tradingview-widget-container">
+            <div id="tradingview_chart"></div>
+            <script type="text/javascript" src="https://s3.tradingview.com/tv.js"></script>
+            <script type="text/javascript">
+            new TradingView.widget(
+            {{
+                "width": "100%",
+                "height": 610,
+                "symbol": "{ticker if ticker else 'AAPL'}",
+                "interval": "D",
+                "timezone": "Asia/Kolkata",
+                "theme": "dark",
+                "style": "1",
+                "locale": "en",
+                "toolbar_bg": "#f1f3f6",
+                "enable_publishing": false,
+                "range": "{timeframe}",
+                "allow_symbol_change": true,
+                "container_id": "tradingview_chart"
+            }});
+            </script>
+        </div>
+        """,
+        height=610,
+    )
